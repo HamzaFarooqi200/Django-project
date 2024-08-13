e8ea6724cf365833a96c0f40dc29586fbe227e13
HamzaFarooqi200 <105863703+HamzaFarooqi200@users.noreply.github.com>
2024-08-13 16:41:27 +0500

    old

--- model_task/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('thanks/', view=views.HelloWorld.as_view(), name="thanks_view"),
-    path('signup/', view=views.Signup.as_view()),
-    path('login/', view=views.Login.as_view(), name= "login"),
-    path('check/', view=views.checkAuthentication),
-]
